45e41645e33c26207d63e56f7ab5d9ffbc5adb9a
D. Khuê Lê-Huu <netw0rkf10w@users.noreply.github.com>
2019-10-01 10:43:29 +0200

    update benchmark

--- sparse_matvec_product.py
+++ sparse_matvec_product.py
@@ -23,17 +23,23 @@ def matvec_fast(x, Adata, Aindices, Aindptr, Ashape):
 
 
 def benchmark(m=10000, n=10000, density=0.1):
-    A = scipy.sparse.random(m, n, density=density, format='csr')
-    x = np.random.randn(A.shape[1])
-    start = time.time()
-    Ax = A.dot(x)
-    print('scipy csr took', time.time() - start, '(s)')
+    for i in range(5):
+        m += 100*i
+        n += 100*i
+        A = scipy.sparse.random(m, n, density=density, format='csr')
+        x = np.random.randn(A.shape[1])
 
-    start = time.time()
-    AxCheck = matvec_fast(x, A.data, A.indices, A.indptr, A.shape)
-    print('matvec_fast took', time.time() - start, '(s)')
+        start = time.time()
+        Ax = A.dot(x)
+        scipy_time = time.time() - start
 
-    print('norm =', np.linalg.norm(Ax - AxCheck))
+        start = time.time()
+        AxCheck = matvec_fast(x, A.data, A.indices, A.indptr, A.shape)
+        matvec_time = time.time() - start
+
+        print('{}) scipy: {:6f} (s), matvec: {:6f} (s)'.format(i + 1, scipy_time, matvec_time))
+
+        # print('norm =', np.linalg.norm(Ax - AxCheck))
 
 if __name__ == "__main__":
     benchmark(m=10000, n=10000, density=0.01)
